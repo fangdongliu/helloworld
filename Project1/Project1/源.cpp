27bd56469b561e98d356d82ecb9d7e301db9807a
Habiten <894856599@qq.com>
2018-06-15 22:44:44 +0800

    efr

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -1,6 +1,29 @@
 #include<stdio.h>
+class B {
+public:
+	int t;
+	template<typename T>
+	void func(T f) {
+		f();
+	}
+	//using gg = decltype(B::func);
+	void ff() {
 
+	}
+};
+
+auto func(int a) {
+	return [a]() {
+		printf("%d\n", a);
+	};
+}
 
 int main() {
+	B b;
+	b.func([]() {
+		printf("1234");
+	});
+	func(2)();
 	printf("1234");
+	getchar();
 }
